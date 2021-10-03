663880e1a4d2fcac41e03b9c5fc73e475ddbb17f
Milanl02 <89406051+Milanl02@users.noreply.github.com>
2021-10-03 00:57:43 -0400

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+var wrapperEle=document.body.querySelector(".wrapper");
+
+var warriors= [
+
+    {
+        name:"Bob",
+        damage:2,
+        health:10,
+        warrior:true
+    },
+    {
+        name:"Jerry",
+        damage:1,
+        health:12,
+        warrior:true
+    },
+    {
+        name:"Mavis",
+        damage:2,
+        health:10,
+        warrior:true
+    },
+    {
+        name:"Morty",
+        damage:4,
+        health:10,
+        warrior:true
+    },
+    {
+        name:"Shorty",
+        damage:10,
+        health:3,
+        warrior:false
+    },
+    {
+        name:"Porty",
+        damage:1,
+        health:14,
+        warrior:true
+    },
+    {
+        name:"Perry",
+        damage:2,
+        health:9,
+        warrior:true
+    },
+    {
+        name:"Larry",
+        damage:2,
+        health:17,
+        warrior:false
+    }
+];
+
+for(var i=0; i<warriors.length; i++) {
+    var person = document.createElement("div");
+    var namePerson = document.createElement("h2");
+    var damagePerson = document.createElement("h4");
+    var healthPerson = document.createElement("h4");
+    var warriorPerson = document.createElement("h4");
+    if (warriors[i].damage >= 2 && warriors[i].health >= 10 && warriors[i].warrior === true) {
+        namePerson.innerHTML = "Name: " + warriors[i].name;
+        damagePerson.innerHTML = "Damage: " + warriors[i].damage;
+        healthPerson.innerHTML = "Health: " + warriors[i].health
+        warriorPerson.innerHTML = "Warrior: " + warriors[i].warrior;
+    } else if(warriors[i].name.includes("a")||warriors[i].name.includes("A")){
+        var element = document.getElementById(warriorPerson);
+        element.style.color = "#FF0000"
+    }
+
+    person.appendChild(namePerson);
+    person.appendChild(damagePerson);
+    person.appendChild(healthPerson);
+    person.appendChild(warriorPerson);
+    wrapperEle.appendChild(person);
+}
+
